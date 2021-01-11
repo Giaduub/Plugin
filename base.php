cf1e2b23d3da363bf0728de74f15c7f3dc67d886
Giaduub <g.dubief@codeur.online>
2021-01-11 15:28:56 +0100

    first

---
+++ base.php
@@ -0,0 +1,12 @@
+<?php 
+/*
+Plugin Name: Covid Tracker
+Plugin URI: LIEN VERS LE PLUGIN
+Description: Plugin Covid Tracker France
+Version: 1.0
+Author: Gianni
+Author URI: LIEN VERS VOTRE PAGE D’ACCUEIL 
+License: GPLv2
+*/
+?>
+
